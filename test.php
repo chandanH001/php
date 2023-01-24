c325deec50a1641e0fbf336025a74bdcc44ae1c3
chandanH001 <chandan.halder96@gmail.com>
2023-01-25 01:40:25 +0600

    first commit

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+
+$num = -100;
+// Using if statement
+if ($num > 0) {
+$result = 'positive';
+}
+elseif ($num < 0) {
+$result = "negative";
+if( $num < -10) {
+$result = "value is below -10";
+}
+}
+else {
+$result = "its zero";
+}
+echo $result;
+
+
+
+
+
