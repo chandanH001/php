18f4cfabec7e8025d3565bdcf72fa16a95163263
chandanH001 <105099116+chandanH001@users.noreply.github.com>
2023-02-06 10:35:32 +0600

    Add files via upload

---
+++ assigment2.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$tuition_fee = 18500;    
+
+$twenty_five_percent = $tuition_fee * 0.25;  
+$twenty_percent = $tuition_fee * 0.2;      
+$fifteen_percent = $tuition_fee * 0.15;     
+$Less = "Invalid";                  
+
+//calculation
+$commission = ($tuition_fee >= 20000) ? ($twenty_five_percent) : (($tuition_fee > 10000 && $tuition_fee < 20000) ? ($twenty_percent) : (($tuition_fee < 10000 && $tuition_fee > 7000) ? ($fifteen_percent) : $invalid));
+
+echo $commission;
